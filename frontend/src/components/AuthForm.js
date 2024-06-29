af553ce460689435f2f32ad1204373f74b9b6001
Christophar Antony <christopharantony@gmail.com>
2024-06-29 21:10:53 +0530

    Implementing auth action

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,16 +1,31 @@
-import { Form, Link, useSearchParams } from "react-router-dom";
+import {
+  Form,
+  Link,
+  useActionData,
+  useNavigation,
+  useSearchParams,
+} from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
 
 function AuthForm() {
   const [searchParams] = useSearchParams();
+  const navigation = useNavigation();
+  const data = useActionData();
 
-  const isLogin = searchParams.get("mode") === "login";
+  const mode = searchParams.get("mode") || "login";
+  const isLogin = mode === "login";
+  const isSubmitting = navigation.state === "submitting";
 
   return (
     <>
       <Form method="post" className={classes.form}>
         <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
+        {data?.errors &&
+          Object.keys(data.errors)?.map((error) => (
+            <li key={error}>{data.errors[error]}</li>
+          ))}
+        {data?.message && <p>{data.message}</p>}
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
@@ -20,10 +35,12 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <Link to={`?mode=${isLogin ? "sigup" : "login"}`}>
+          <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
             {isLogin ? "Create new user" : "Login"}
           </Link>
-          <button>Save</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Submitting..." : "Save"}
+          </button>
         </div>
       </Form>
     </>
